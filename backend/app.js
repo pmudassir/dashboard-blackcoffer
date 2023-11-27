75f58380e07c7bd5e6e0bcf37b58dab6dd484efe
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2023-11-27 23:54:51 +0530

    initial setup

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./utils/db');
+require("dotenv").config()
+
+const app = express();
+
+app.use(cors());
+
+connectDB()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
